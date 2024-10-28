e828937128cb40438e5162b38c557b00e4f2b733
sinisti <spideralan2008@gmail.com>
2024-10-28 09:01:01 +0100

    Colors

--- app/src/main/java/com/example/gambling/MainActivity.java
+++ app/src/main/java/com/example/gambling/MainActivity.java
@@ -5,16 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_layout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
 }
+int random1 = rand.nextInt(100) + 1; //generate random numbers from 1-100
+
+n1.setText(Integer.toString(random1)); //n1 is a TextView
